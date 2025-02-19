5820c2f2da29e0baea1d69851ee74854cf315168
arjunm100 <arjunm10011@gmail.com>
2025-02-19 13:45:10 +0530

    automated backup

--- Backup_Cron/backup.sh
+++ Backup_Cron/backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-DB_CONTAINER_ID="odoo-docker-setup_db_main_1"
+# docker exec 68e25c4e0d8a pg_dump -U main DB1 > /home/cybrosys/Desktop/Docker/odoo-docker-setup/Backup_Cron/backup_data/odoo_db_backup_$(date +"%Y-%m-%d_%H-%M-%S").sql
+
+DB_CONTAINER_ID="68e25c4e0d8a"
 
 CONTAINER_ID="odoo-docker-setup_odoo_main_1"
 
@@ -9,7 +11,7 @@ HOST_PATH="/home/cybrosys/Desktop/Docker/odoo-docker-setup/Backup_Cron/backup_da
 FILE_NAME="odoo_data_backup_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz"
 
 # Database Backup
-docker exec -t $DB_CONTAINER_ID pg_dumpall -c -U main > $HOST_PATH/odoo_db_backup_$(date +"%Y-%m-%d_%H-%M-%S").sql
+docker exec 68e25c4e0d8a pg_dump -U main DB1 > $HOST_PATH/odoo_db_backup_$(date +"%Y-%m-%d_%H-%M-%S").sql
 # sudo docker exec -t 312c8ec94f46 pg_dumpall -c -U odoo18 > dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql
 
 docker exec odoo-docker-setup_odoo_main_1 tar -cvzf tmp/$FILE_NAME -C /var/lib odoo
