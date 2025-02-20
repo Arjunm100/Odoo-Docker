db0221348639d7ad6bea1cba30e31b5b5262e57d
arjunm100 <arjunm10011@gmail.com>
2025-02-20 12:45:30 +0530

    backup for volumes

--- Backup_Cron/backup.sh
+++ Backup_Cron/backup.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
 
-DB_CONTAINER_ID="odoo-docker-setup_db_main_1"
+DB_VOLUME="odoo-docker-setup_db_main"
 
-CONTAINER_ID="odoo-docker-setup_odoo_main_1"
+ODOO_VOLUME="odoo-docker-setup_odoo-main-data"
 
 HOST_PATH="/home/cybrosys/Desktop/Docker/odoo-docker-setup/Backup_Cron/backup_data"
 
 LOG_PATH="/home/cybrosys/Desktop/Docker/odoo-docker-setup/Backup_Cron/logs"
 
-find $HOST_PATH -type f -name '*.tar.gz' -mmin +5 -exec rm {} \;
+find $HOST_PATH -type f -name '*.tar' -mmin +1 -exec rm {} \;
 
-find $HOST_PATH -type f -name '*.sql' -mmin +5 -exec rm {} \;
+sudo find $LOG_PATH -type f -name '*.log' -mmin +5 -exec rm {} \;
 
-sudo find $LOG_PATH -type f -name '*.log' -mmin +10 -exec rm {} \;
+docker run --rm -v "$DB_VOLUME:/data" -v "$HOST_PATH:/backup" busybox sh -c 'tar czf /backup/db_backup_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz /data'
 
-RESULT=$(sudo docker ps --format "{{.Names}}")
-
-string='My long string'
-if [[ $RESULT == *"$CONTAINER_ID"*  &&  $RESULT == *"$DB_CONTAINER_ID"* ]]; then
-    FILE_NAME="odoo_data_backup_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz"
-
-    docker exec $DB_CONTAINER_ID pg_dump -U main DX > $HOST_PATH/odoo_db_backup_$(date +"%Y-%m-%d_%H-%M-%S").sql
-
-    docker exec odoo-docker-setup_odoo_main_1 tar -cvzf tmp/$FILE_NAME -C /var/lib odoo
-
-    docker cp $CONTAINER_ID:tmp/$FILE_NAME $HOST_PATH
-
-    docker exec $CONTAINER_ID sh -c 'rm /tmp/*.tar.gz'
-else
-    echo "Containers are not online"
-fi
+docker run --rm -v "$ODOO_VOLUME:/data" -v "$HOST_PATH:/backup" busybox -c 'tar cvf /backup/odoo_backup_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz /data'
